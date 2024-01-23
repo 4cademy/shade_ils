cd67f77f908560d9e582f9a5ac83c01b20b32a7b
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2024-01-23 16:15:14 +0100

    Implemented objective function 2

--- objective_functions.h
+++ objective_functions.h
@@ -5,7 +5,6 @@
 #ifndef SHADE_ILS_OBJECTIVE_FUNCTIONS_H
 #define SHADE_ILS_OBJECTIVE_FUNCTIONS_H
 
-float objective_function_1(const std::vector<float>& solution, int DIM);
 float objective_function_no(const std::vector<float>& solution, int DIM, int number);
 
 #endif //SHADE_ILS_OBJECTIVE_FUNCTIONS_H
